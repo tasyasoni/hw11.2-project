4932f00090efe8d918b5d1ad9331713d503a6350
tasyasoni <tasyasoni1@gmail.com>
2023-07-25 00:02:38 +0300

    'changed func'

--- str_func.py
+++ str_func.py
@@ -1,7 +1,18 @@
 def strwords (value):
+    """
+    докстирнг в локальной для конфликта
+    """
     return(value.upper())
 
 print(strwords("страна моя родная"))
 
+
+def strwords (value):
+    """
+    докстирнг в локальной для конфликта
+    """
+    return(value.title())
+
+print(strwords("страна моя родная"))
 
 
